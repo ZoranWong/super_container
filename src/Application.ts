59210efdb6aba098d2d740b3dc7325bd048f9809
wong1989 <skyandsunning@163.com>
2019-04-10 10:01:59 +0800

    add framework adpater

--- src/Application.ts
+++ src/Application.ts
@@ -3,21 +3,23 @@ import CommandInterface from './contracts/CommandInterface';
 import Container from "./container/Container";
 import FrameworkInstanceAdapter from "./contracts/FrameworkInstanceAdapterInterface";
 import EventListenerInterface from "./contracts/EventListenerInterface";
+import RouterAdapterInterface from "./contracts/RouterAdapterInterface";
 
 export default class Application extends Container {
     public static version = '0.0.1';
-    private providersContainer: Set<any> = null;
+    private providersContainer: Set<ServiceProvider> = null;
     private showingPageObject: FrameworkInstanceAdapter = null;
     private configs: Map<string, any> = null;
     private dispatcher: EventListenerInterface = null;
     private commandPrefix = 'COMMAND:';
+    private router: RouterAdapterInterface = null;
 
     /*
     * Application 构造函数
     * */
     public constructor () {
         super();
-        this.providersContainer = new Set();
+        this.providersContainer = new Set<ServiceProvider>();
         this.configs = new Map();
         this.dispatcher = this.configs.get('app.dispatcher');
     }
@@ -48,13 +50,25 @@ export default class Application extends Container {
             return command;
         });
     }
-    public pageChange (route: string) {
-        this.showingPageObject = this.make(this.pageKey(route));
+
+    public redirect (route: string) {
+        this.showingPageObject = this.get(this.pageKey(route));
     }
 
-    public addPage (route: string, pageObject: any) {
-        let adapter = this.configs.get('app.pageAdapter');
-        this.singleton(this.pageKey(route), new adapter(pageObject))
+    public addRoute (route: string, options: any = null) {
+        let multiple = this.configs.get('app.multiple');
+        if (multiple) {
+            let adapter = this.configs.get('app.pageAdapter');
+            let instance = this.buildPage(adapter, options);
+            this.singleton(this.pageKey(route), instance);
+        }
+        this.router.push(route, options);
+    }
+
+    private buildPage (adapter: any, options: any = null): FrameworkInstanceAdapter {
+        let instance: FrameworkInstanceAdapter = new adapter(options);
+        instance.mount();
+        return instance;
     }
 
     private pageKey (route: string): string {
@@ -62,9 +76,10 @@ export default class Application extends Container {
     }
 
     public run () {
-        this.providersContainer.forEach((provider: any) => {
+        this.providersContainer.forEach((provider: ServiceProvider) => {
             if ('register' in provider)
                 provider.register();
         });
+        this.showingPageObject.mount();
     }
 }
